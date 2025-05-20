a9986e027e7fdf6e8c347f9f4a521833ef88ddfe
Kacper Jasinski <kacperjasinski78@gmail.com>
2025-05-20 22:05:07 +0200

    Stworzenie klasy Piece

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def main():
-    print("Projekt Warcaby!")
-    print("Testy")
+class Piece:
+    def __init__(self, color, king = False):
+        self.color = color 
+        self.king = king
+
+    def __str__(self):
+        return self.color.upper() if self.king else self.color
