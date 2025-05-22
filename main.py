f984c8cf64250d49e7fecb3c16d839107e261728
Kacper Jasinski <kacperjasinski78@gmail.com>
2025-05-22 14:37:08 +0200

    zaczęto pracę nad klasą CheckersGUI oraz dodano bibliotekę tkinter

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+import tkinter as tk
+
 class Piece:
     def __init__(self, color, king = False):
         self.color = color 
         self.king = king
 
-    def __str__(self):
-        return self.color.upper() if self.king else self.color
+class CheckersGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Warcaby")
+        self.canvas = tk.Canvas(root, width = 640, height = 640)
+        self.canvas.pack()
+        self.board = [[None for _ in range(8)] for _ in range(8)]
+        self.turn = 'w'
+        self.selected = None
+        self.valid_moves = {}
+        self.must_continue_capture = False
+        self.capture_origin = None
+        self.init_board()
+        self.canvas.bind("<Button-1>", self.click)
+        self.draw_board()
