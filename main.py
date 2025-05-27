e001cee3cb2d7667941c49f88f8a8ae3b7f760d1
Kacper Jasinski <kacperjasinski78@gmail.com>
2025-05-27 11:58:04 +0200

    Prace nad klasą CheckersGUI, stworzenie funkcji draw_board

--- main.py
+++ main.py
@@ -27,10 +27,19 @@ class CheckersGUI:
                 if (row + col) % 2 == 1:
                     self.board[row][col] = Piece('b')
         for row in range(6, 8):
-            for col in range(8):
+            for col in ra   nge(8):
                 if (row + col) % 2 == 1:
                     self.board[row][col] = Piece('w')
 
-    
+    def draw_board(self):
+        self.canvas.delete("all")
+        colors = ["#EEE", "#444"]
+        for r in range(8):
+            for c in range(8):
+                x1, y1 = c * 80, r * 80
+                x2, y2 = x1 + 80, y1 + 80
+                self.canvas.create_rectangle(x1, y1, x2, y2, fill = colors[(r + c) % 2])
+                piece = self.board[r][c]
+
     def in_bounds(self, r, c):
         return 0 <= r < 8 and 0 <= c < 8
